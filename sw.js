9573413f1f93f09b68d74a540e69d8828217e0af
MEKKIS <marcus.aas.mekiassen@gmail.com>
2026-05-13 23:31:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,39 @@
 const CACHE_NAME = 'min-hub-v6';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
+
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
 });
+
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
-    const copy = resp.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
-    return resp;
-  }).catch(() => caches.match('./index.html'))));
+
+  event.respondWith(
+    fetch(event.request)
+      .then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, copy))
+          .catch(() => {});
+        return resp;
+      })
+      .catch(() =>
+        caches.match(event.request)
+          .then(cached => cached || caches.match('./index.html'))
+      )
+  );
 });
